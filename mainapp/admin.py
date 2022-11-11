a88a35a99e6b072d548b0adcc693bb0fb9937391
GarrikDanilov <garrik_danilov@mail.ru>
2022-11-11 03:20:11 +0300

    add models and migrate

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from mainapp.models import News, Course, Lesson, CourseTeachers
 
-# Register your models here.
+
+admin.site.register(News)
+admin.site.register(Course)
+admin.site.register(Lesson)
+admin.site.register(CourseTeachers)
